c04de00813bbeebd06025d7ed5685f0cc4ced346
Yeonwoo123456 <youngist06@gmail.com>
2026-06-23 10:57:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ Keep under 2 sentences.
 
             try:
                 response = client.models.generate_content(
-                    model = "gemini-1.5-flash-002",
+                    model = "gemini-2.5-flash",
                     contents=[prompt]
                 )
 
