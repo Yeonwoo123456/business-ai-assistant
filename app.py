6f1b7f0b076545e52f1f90298df86c6550875e45
Yeonwoo123456 <youngist06@gmail.com>
2026-06-23 10:44:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ if menu == "Business Location Recommendation":
         with st.spinner("Generating recommendation..."):
 
             response = client.models.generate_content(
-                model="gemini-2.5-flash",
+                model="gemini-1.5-flash",
                 contents=prompt
             )
 
